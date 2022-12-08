00863dda2f51a346f8ffc51e48865889e564e2a0
Angelpola <112270366+Angelpola@users.noreply.github.com>
2022-12-07 21:04:21 -0600

    bd 9pm
    configuracion

--- public/src/settings.php
+++ public/src/settings.php
@@ -57,7 +57,7 @@ if($dev_inef == true) {
             'db' => [
                 'host' => '34.68.63.8', // Host del servidor al que se conecta la base de datos
                 'user' => 'inef-bd', // Nombre de usuario de la base de datos
-                'pass' => '7PsW6m43ARNkvgl', // Contraseña de la base de datos
+                'pass' => '1gS{kLQ]>tJS3<g', // Contraseña de la base de datos
                 'dbname' => 'plat_inef' // Nombre de la base de datos a la que se conecta la plataforma
             ],
             // Renderer settings
