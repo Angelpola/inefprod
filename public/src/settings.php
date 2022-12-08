408229b63c8cc40fd5bb05c58a81f71c29272b0d
Angelpola <112270366+Angelpola@users.noreply.github.com>
2022-12-07 20:34:58 -0600

    colores inef
    colores inef api

--- public/src/settings.php
+++ public/src/settings.php
@@ -56,7 +56,7 @@ if($dev_inef == true) {
             //Config DB
             'db' => [
                 'host' => '34.68.63.8', // Host del servidor al que se conecta la base de datos
-                'user' => 'fldch', // Nombre de usuario de la base de datos
+                'user' => 'inef-bd', // Nombre de usuario de la base de datos
                 'pass' => '7PsW6m43ARNkvgl', // Contraseña de la base de datos
                 'dbname' => 'plat_inef' // Nombre de la base de datos a la que se conecta la plataforma
             ],
